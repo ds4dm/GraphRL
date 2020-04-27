4aa36ac122399e40bae020171b8be5f61a9de436
Defeng <pandat8@gmail.com>
2020-04-27 03:36:58 -0400

    upd

--- supervised/train_supervised_learning.py
+++ supervised/train_supervised_learning.py
@@ -791,11 +791,11 @@ class Train_SupervisedLearning:
 
             if self.use_cuda:
                 self.model.load_state_dict(
-                    torch.load('./supervised/models/'+self.heuristic+'/SmallErgTraining/lrs'+str(self.lr)+'/per_epochs/gcn_policy_' + self.heuristic + '_pre_' + self.train_dataset.__class__.__name__
+                    torch.load('./supervised/models/'+self.heuristic+'/SmallErgTraining/lrss'+str(self.lr)+'/per_epochs/gcn_policy_' + self.heuristic + '_pre_' + self.train_dataset.__class__.__name__
                            + '_epochs_' + str(epoch) + '_cuda.pth'))
             else:
                 self.model.load_state_dict(
-                    torch.load('./supervised/models/' + self.heuristic + '/SmallErgTraining/lrs' + str(
+                    torch.load('./supervised/models/' + self.heuristic + '/SmallErgTraining/lrss' + str(
                         self.lr) + '/per_epochs/gcn_policy_' + self.heuristic + '_pre_' + self.train_dataset.__class__.__name__
                                + '_epochs_' + str(epoch) + '_cuda.pth', map_location='cpu'))
 
