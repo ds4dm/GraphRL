4adb683755270023d665b851a63bff52169b64a4
Defeng <pandat8@gmail.com>
2020-05-18 01:35:50 -0400

    upd

--- supervised/train_supervised_learning.py
+++ supervised/train_supervised_learning.py
@@ -2318,16 +2318,16 @@ class Train_SupervisedLearning:
                         # m = Categorical(logits=output) # logits=probs
                         # action_gcn = m.sample()
 
-                        # action_gcn = output.argmax()
+                        action_gcn = output.argmax()
 
                         # output = np.array(output.detach().cpu().numpy())
                         # output = np.exp(output)
                         # action_gcn = np.argmax(output)
 
-                        if self.heuristic == 'min_degree':
-                            action_gcn, d_min = x_model.min_degree(x_model.M)
-                        elif self.heuristic == 'one_step_greedy':
-                            action_gcn = x_model.onestep_greedy()
+                        # if self.heuristic == 'min_degree':
+                        #     action_gcn, d_min = x_model.min_degree(x_model.M)
+                        # elif self.heuristic == 'one_step_greedy':
+                        #     action_gcn = x_model.onestep_greedy()
 
                         _t2 = time.clock()
                         edges_added = x_model.eliminate_node(action_gcn, reduce=True)
@@ -2408,7 +2408,7 @@ class Train_SupervisedLearning:
               'KLlossExp2GNN_SSsmall {}'.format(av_loss_exp2GNNss),
               'KLlossExp2Random {}'.format(av_loss_exp2rand),
               # 'MF_performance {}'.format(_val_ave_gcn)
-              'GNN_performance {}'.format(_val_ave_gcn)
+              'GNN_ERsmall_performance {}'.format(_val_ave_gcn)
               # self.heuristic+'_performance {}'.format(_val_ave_mind),
               # 'gcn_performance {}'.format(_val_ave_gcn)
               )
